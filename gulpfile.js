281e6449bd600b6a8ed9956d9e33809498c70f88
Andrea Barghigiani <a.barghigiani@gmail.com>
2020-01-25 17:08:33 +0100

    installato e configurato gulp-autoprefix

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { dest, src } = require("gulp");
 const sass = require("gulp-dart-sass");
+const prefix = require("gulp-autoprefixer");
 
 function scss() {
 	return src("scss/**/*.scss")
 		.pipe(sass())
+		.pipe(prefix())
 		.pipe(dest("./"));
 }
 
