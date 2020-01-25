6ca0d956fc97be651efd389b68f4b9cfea49630d
Andrea Barghigiani <a.barghigiani@gmail.com>
2020-01-25 17:10:25 +0100

    installato e configurato gulp-sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,15 @@ const { dest, src } = require("gulp");
 const sass = require("gulp-dart-sass");
 const prefix = require("gulp-autoprefixer");
 const csso = require("gulp-csso");
+const sourcemaps = require("gulp-sourcemaps");
 
 function scss() {
 	return src("scss/**/*.scss")
+		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(prefix())
 		.pipe(csso())
+		.pipe(sourcemaps.write("."))
 		.pipe(dest("./"));
 }
 
