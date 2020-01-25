7692f18aa088cc9392558f41c039d517042d2967
Andrea Barghigiani <a.barghigiani@gmail.com>
2020-01-25 17:09:22 +0100

    installato gulp-csso

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const { dest, src } = require("gulp");
 const sass = require("gulp-dart-sass");
 const prefix = require("gulp-autoprefixer");
+const csso = require("gulp-csso");
 
 function scss() {
 	return src("scss/**/*.scss")
 		.pipe(sass())
 		.pipe(prefix())
+		.pipe(csso())
 		.pipe(dest("./"));
 }
 
